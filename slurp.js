83e5a2168602dce5fc9229b64238600f35550673
adkatrit <jarrodrparker@gmail.com>
2013-07-24 22:14:29 -0400

    cleanup

--- slurp.js
+++ slurp.js
@@ -1,72 +1,51 @@
-
 /**
  * Module dependencies.
  */
 
-  var spotify = require('spotify.js')
-  , redis = require('redis').createClient()
-  , http = require('http');
+var spotify = require('spotify.js')
+, redis = require('redis').createClient()
+, http = require('http');
 
 var urban_random = 'http://www.urbandictionary.com/random.php';
 
-
+//get a random word from urbandictionary
 function get_urb(cb){
+  http.get(urban_random, function(res) {
+    var spl = res.headers.location.split('=');
+    var rand = spl[spl.length-1];
+    cb(rand);
 
-
-
- http.get(urban_random, function(res) {
-
-   var spl = res.headers.location.split('=');
-
-   var rand = spl[spl.length-1];
-
- cb(rand);
-
- }).on('error', function(e) {
+  }).on('error', function(e) {
    console.log("Got error: " + e.message);
- });
-}
-     
-
-process.on('uncaughtException',console.log);
-
-function slurp_artist(name){
-  spotify.artists( name, function(err, artists) {
-    console.log('Artists results:', artists);
   });
-
 }
 
+//store a word in the total_rando set
+function store_in_set(track){
+  if(typeof track.href !='undefined'){
+    if(track.album.availability.territories.indexOf('US') != -1){
+      redis.sadd(  'total_rando' , track.href.split(':')[2], function(){});  
+    }
+  }
+}
+//stream random spotify search results to redis
 function slurp_search(){
   get_urb(function(rand){
-  console.log(rand);
-  try{
-    var timeout = 0;
-    var done = false;
-    spotify.tracks(rand).forEach(function(track) {
-	clearTimeout(timeout);
-	timeout=setTimeout(function(){
-		done=true;
-	},20);
-
-      if (track === null) {
-          // finished
-      } else {
-        if(typeof track.href !='undefined'){
-          if(track.album.availability.territories.indexOf('US') != -1){
-            redis.sadd(  'total_rando' , track.href.split(':')[2], function(){});  
-		if(done){
-			slurp_search();
-		}
-          }
+    try{
+      var timeout = 0;
+      var done = false;
+      spotify.tracks(rand).forEach(function(track) {
+        if (track === null) {
+            slurp_search();
+        } else {
+          store_in_set(track);
         }
-      }
-    });
-  }catch(e){
-   console.log(e);
- }
-
-});
+      });
+    }catch(e){
+     console.log(e);
+    }
+  });
 }
 
+process.on('uncaughtException',console.log);
 slurp_search();
